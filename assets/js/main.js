4041fc2b17568834b76c3579820100b69db841ff
preetiverma123 <preetiverma123>
2019-02-07 18:59:08 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -239,13 +239,3 @@ $('.share_with a').on('click',function(){
   $('.social-icon').toggleClass('open');
   $('.share_with').toggleClass('open');
 })
-$('#about-slider').owlCarousel({
-    items:1,
-    loop:true,
-    margin:15,
-    nav: true,
-    navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-    dots : true,
-    autoplay : true,
-    animateOut: 'fadeOut'
-  });
