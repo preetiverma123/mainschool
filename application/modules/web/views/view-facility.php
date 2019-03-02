2f23ac56ea132e9d1c08e3475ed84cd901570465
preetiverma123 <preetiverma123>
2019-03-02 12:40:06 +0530

    changes

--- application/modules/web/views/view-facility.php
+++ application/modules/web/views/view-facility.php
@@ -66,9 +66,64 @@
 <section class="facilitySection">
   <div class="container">
     <div class="col-md-12">
-      <div>
+      <div class="facilityWrapper">
         <div class="facilityHead">
           <h2>Bus Facility</h2>
+        </div>
+        <div class="facilityContent">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <div class="row">
+            <div class="col-md-4">
+              <ul>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Bus facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Smart Classes</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Yoga facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Canteen facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Bus facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Smart Classes</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Yoga facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Canteen facility</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <ul>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Bus facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Smart Classes</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Yoga facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Canteen facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Bus facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Smart Classes</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Yoga facility</li>
+                <li><i class="fa fa-check-square-o" aria-hidden="true"></i>Canteen facility</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <div class="facilityImg">
+                <img src="assets/images/school-bus.jpg" alt="facility">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- second facility -->
+    <div class="col-md-12">
+      <div class="facilityWrapper">
+        <div class="facilityHead">
+          <h2>Bus Facility</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-7">
+            <div class="facilityContent">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="facilityImg">
+              <img src="assets/images/library.jpg" alt="facility">
+            </div>
+          </div>
         </div>
       </div>
     </div>
